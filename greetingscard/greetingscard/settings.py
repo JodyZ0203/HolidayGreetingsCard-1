74aec7c7761e92d721f809eacf5f646c71447d07
JodyZ0203 <Jody625326@gmail.com>
2020-12-25 17:13:05 -0500

    setting up the frontend

--- greetingscard/greetingscard/settings.py
+++ greetingscard/greetingscard/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'card.apps.CardConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
--- greetingscard/greetingscard/urls.py
+++ greetingscard/greetingscard/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('card.urls'))
+    path('', include('card.urls')),
+    path('', include('frontend.urls'))
 ]
