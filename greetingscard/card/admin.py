4fce09fc8eae190c3889188d05d7cd02666af3cd
JodyZ0203 <Jody625326@gmail.com>
2020-12-26 13:19:41 -0500

    commit on main

--- greetingscard/card/admin.py
+++ greetingscard/card/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CardInfo
 
 # Register your models here.
+admin.site.site_header = 'Proxima Centauri Admin'
+admin.site.site_title = 'Holiday Greetings Card Generator Admin Area'
+
+
+admin.site.register(CardInfo)
--- greetingscard/pages/views.py
+++ greetingscard/pages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from card.models import CardInfo
 # Create your views here.
@@ -8,3 +8,7 @@ def myView(request):
     print(greet_settings)
     return render(request, 'index.html',
     {'all_items': greet_settings})
+
+def results(request, id):
+    picture = get_object_or_404(CardInfo, pk=id)
+    return render(request, 'card/results.html', {'picture': picture})
