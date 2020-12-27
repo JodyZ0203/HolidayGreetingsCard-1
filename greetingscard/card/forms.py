23e56abe5603b008ec0a19ea38a7dfd418042493
JodyZ0203 <Jody625326@gmail.com>
2020-12-26 20:00:47 -0500

    commit on main

--- greetingscard/card/forms.py
+++ greetingscard/card/forms.py
@@ -5,4 +5,4 @@ from .models import CardInfo
 class CardFields(forms.ModelForm):
     class Meta:
         model = CardInfo
-        fields = ('id', 'Holiday_Choice', 'Recipient_Name', 'Recipient_Location', 'Short_Message', 'Your_Name')
+        fields = ('id', 'Holiday_Choice', 'Recipient_Name', 'Recipient_Location','Your_Name')
