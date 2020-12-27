9203562649c51e8daecd579d9dbcf5bf71194ced
JodyZ0203 <Jody625326@gmail.com>
2020-12-26 20:23:56 -0500

    commit to main

--- greetingscard/greetingscard/settings.py
+++ greetingscard/greetingscard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8j+#(e@&-88bn02+2^_-w4daurp@7+7=^&)$$spkys$fh&9taz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
